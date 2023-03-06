7ef8f35b73d593620931b0e882abd300ed877403
Valeria <nelera.gb@gmail.com>
2023-03-06 11:14:36 +0300

    Реализовать простой калькулятор (+ - / *)

--- JAVA_HOMEWORK/homework001/task003.java
+++ JAVA_HOMEWORK/homework001/task003.java
@@ -5,24 +5,27 @@
 Ввод числа ->
  */
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class task003 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите первое число: ");
-        double num01 = input.nextDouble();
+        float num01 = Float.parseFloat(input.nextLine().replace(",", "."));
         System.out.print("Введите операцию (+ - / *): ");
-        char operation = input.next().charAt(0);
+        char operation = input.nextLine().charAt(0);
         System.out.print("Введите второе число: ");
-        double num02 = input.nextDouble();
+        float num02 = Float.parseFloat(input.nextLine().replace(",", "."));
         input.close();
-        calc(num01, num02, operation);
+        System.out.println(calc(num01, num02, operation));
     }
 
-    public static void calc(double number01, double number02, char operation) {
-
-        double result = 0;
+    public static String calc(float number01, float number02, char operation) {
+        float result = 0;
+        StringBuilder resString = new StringBuilder();
+        NumberFormat numFormat = new DecimalFormat("#.######");
         if (operation == '+') {
             result = number01 + number02;
         } else if (operation == '-') {
@@ -32,28 +35,16 @@ public class task003 {
         } else if (operation == '/') {
             result = number01 / number02;
         } else {
-            System.out.println("Вы ввели недоступную операцию");
+            resString.append("Операция недоступна\n");
         }
         if (operation == '+' || operation == '-' || operation == '*' || operation == '/') {
-            if (number01 % 1 == 0) {
-                int inumber01 = (int) number01;
-                System.out.print(inumber01);
-            } else {
-                System.out.printf("%.2f", number01);
-            }
-            System.out.printf(" %s ", operation);
-            if (number02 % 1 == 0) {
-                int inumber02 = (int) number02;
-                System.out.printf("%d = ", inumber02);
-            } else {
-                System.out.printf("%.2f = ", number02);
-            }
-            if (result % 1 == 0) {
-                int iresult = (int) result;
-                System.out.println(iresult);
-            } else {
-                System.out.printf("%.2f\n", result);
-            }
+            resString.append(numFormat.format(number01))
+                    .append(" ").append(operation).append(" ")
+                    .append(numFormat.format(number02))
+                    .append(" = ")
+                    .append(numFormat.format(result))
+                    .append("\n");
         }
+        return resString.toString();
     }
 }
